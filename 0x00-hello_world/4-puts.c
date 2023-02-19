1eaed621e6c53434b1dbfee75299d47e959ca2d4
leomb <leulmerid@gmail.com>
2023-02-19 03:14:52 -0800

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,3 +9,4 @@ int main(void)
 puts("\"programming is like building a multilingual puzzle");
 return (0);
 }
+
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
  * main - c program that prints using
  * printf
  *
- * Return :0
+ * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }

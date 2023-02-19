028dba48d1ce7992a09b4d825645e94e4e40f003
leomb <leulmerid@gmail.com>
2023-02-19 08:32:41 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - program that prints to the standard error
+ *
+ * Return: 1
+ */
+int main(void)
+{
+fput("that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);

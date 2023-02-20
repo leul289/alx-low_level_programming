784c53f7ac33d570399fadbfaaad4bd62302ca75
leomb <leulmerid@gmail.com>
2023-02-20 09:54:48 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - alphabet in reverseb 7
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int r = 122;
+while (r <= 97)
+{
+putchar(r);
+r--;
+}
+putchar('\n');
+return (0);
+}

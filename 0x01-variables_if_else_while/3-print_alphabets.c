e531f863aebc14e7b44b1c2968b2257f0e20fd35
leomb <leulmerid@gmail.com>
2023-02-20 02:17:27 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - upper case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+a = 65;
+while                                    (a <= 90)
+{                                        putchar(a);
+a++;
+}
+putchar('\n');
+return (0);                              }

6a56af366e5fc354ce692b3cc8418b042d0fdba1
leomb <leulmerid@gmail.com>
2023-02-20 08:25:23 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print numbers 6
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+putchar(n + 48);
+}
+putchar('\n');
+return (0);
+}

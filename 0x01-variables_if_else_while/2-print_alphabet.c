485139f4ddf426ad393577549319548eb19e7d1b
leomb <leulmerid@gmail.com>
2023-02-20 01:46:34 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <stdint.h>
 /**
  * main - intery point
  *
  * Return: 0
  */
-int main (void)
+int main(void)
+{
 int b;
-b = "a";
+b = 97;
 while
-(b <= "z") {
+(b <= 122)
+{
 putchar(b);
 b++;
 }
+
 putchar('\n');
 return (0);
+}

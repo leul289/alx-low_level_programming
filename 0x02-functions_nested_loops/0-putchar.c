b7fa82b27b33186390c9f6724eec2b8a471567cc
leomb <leulmerid@gmail.com>
2023-02-20 21:36:51 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+/**
+ * main - Write a program that prints _putchar, followed by a new line.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int h = 104;
+	putchar(h);
+putchar('\n');
+return (0);
+}

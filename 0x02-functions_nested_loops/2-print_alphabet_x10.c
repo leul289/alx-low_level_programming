4bbf77c284212d9da203dd6daa188af55740c4c0
leomb <leulmerid@gmail.com>
2023-02-22 08:52:59 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,20 @@
 #include "main.h"
 /**
- * main - Write a function that prints 10 times the alphabet.
+ * print_alphabet_x10 - Write a function that prints 10 times the alphabet.
  *
  * Return: 0
  */
 void print_alphabet_x10(void)
 {
-	int a = 97;
-	int i = 1;
+	int a;
+	int i;
 
-	while (a <= 122)
+	for (i = 1; i <= 10; i++)
 	{
-	for (i = 0; i <= 10; i++)
+	for (a = 97; i <= 122; i++)
 	{
 	_putchar(a);
 	}
-	_putchar(a);
-	a++;
+	_putchar('\n');
 	}
-_putchar('\n');
 }

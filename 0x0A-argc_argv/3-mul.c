fb9349048d9f35418a294bc0fa99bbd08aaefc14
leomb <leulmerid@gmail.com>
2023-03-16 13:40:29 -0700

    alx

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include "main.h"
 /**
@@ -26,18 +25,19 @@ int _atoi(char *s)
 		if (s[i] == '-')
 		++d;
 
-                if (s[i] >= '0' && s[i] <= '9')
-		{
-		digit = s[i] - '0';
-		if (d % 2)
-		digit = -digit;
-		n = n * 10 + digit;
-		f = 1;
+	if (s[i] >= '0' && s[i] <= '9')
+	{
+	digit = s[i] - '0';
 
-		if (s[i + 1] < '0' || s[i + 1] > '9')
-		break;
-		f = 0;
-		}
+	if (d % 2)
+	digit = -digit;
+	n = n * 10 + digit;
+	f = 1;
+
+	if (s[i + 1] < '0' || s[i + 1] > '9')
+	break;
+	f = 0;
+	}
 		i++;
 	}
 	if (f == 0)

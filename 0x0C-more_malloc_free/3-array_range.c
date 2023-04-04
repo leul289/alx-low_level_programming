21c065d0917b60d232276c5da528e0ffaa6cf92e
leomb <leulmb@example.com>
2023-04-04 01:20:26 -0700

    alx

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -41,8 +41,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size > old_size)
 	{
-		for (i = 0; i < old_size; i++)
-		ptr1[i] = old_ptr[i];
+	for (i = 0; i < old_size; i++)
+	ptr1[i] = old_ptr[i];
 	}
 	free(ptr);
 	return (ptr1);
